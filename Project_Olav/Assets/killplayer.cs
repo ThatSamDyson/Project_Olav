d8fa5f18cd68fbec95ca6b6c4fa2f7a609800f8a
Samuel Dyson <d010941k@student.staffs.ac.uk>
2023-03-19 11:57:27 +0000

    World Build

--- Project_Olav/Assets/killplayer.cs
+++ Project_Olav/Assets/killplayer.cs
@@ -7,18 +7,6 @@ public class killplayer : MonoBehaviour
     public GameObject player;
     public Transform respawnPoint;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter(Collision other)
     {
         if(other.gameObject.CompareTag("Player"))
