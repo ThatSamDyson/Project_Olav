b82e2a4efb66ee33d0681e11cb26fb4440f7752d
Samuel Dyson <d010941k@student.staffs.ac.uk>
2023-04-05 17:55:03 +0100

    5/04/23

---
+++ Project_Olav/Assets/Scripts/Goal.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    private ScoringSystem scoring;
+    public GameObject Score;
+    [SerializeField] public int pointValue;
+
+
+    private void Awake()
+    {
+        scoring = Score.GetComponent<ScoringSystem>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Football"))
+        {
+            scoring.score += pointValue;
+        }
+    }
+}
