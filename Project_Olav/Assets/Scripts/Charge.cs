c066bc574ee04c53423d84f8416b227ad805fb2f
Samuel Dyson <d010941k@student.staffs.ac.uk>
2023-04-17 12:56:35 +0100

    17/04

---
+++ Project_Olav/Assets/Scripts/Charge.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Charge : MonoBehaviour
+{
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
